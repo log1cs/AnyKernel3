aa77e6bd9ce3117c7bb7220c8f02f4d9fe552d8b
Radu Vasilascu <glx33807@gmail.com>
2021-09-03 12:28:56 +0100

    Pull some changes from the umod repo

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string={NAME} Kernel
+kernel.string=umod kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1={DEVICE}
-supported.versions=
+device.name1=NB1
+supported.versions=11
 supported.patchlevels=
 '; } # end properties
 
@@ -25,6 +25,12 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 750 750 $ramdisk/*;
+
+## Trim partitions
+$bin/busybox fstrim -v /data;
 
 ## AnyKernel install
 dump_boot;
